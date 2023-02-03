c6bdfe0ed6a964f993d922caf932762ed66e0db1
amit jhariya <amit.jhariya93@gmail.com>
2023-02-03 16:55:53 +0530

    Add Resume

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Blogs from '../components/blogs';
+import Blogs from '../components/Blogs';
 import Layout from "../components/Layout";
 
 export default function Home({open}) {
