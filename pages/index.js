2f26da1deee7c4d7ca86c72ea4dd8aab58c1c7b4
Amit Jhariya <amit.jhariya@criteriacorp.com>
2023-10-04 20:20:52 +0530

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,34 @@
 import Head from "next/head";
 import Layout from "../components/Layout/Particles";
 import HomeComponent from "../components/Home";
+import axios from 'axios'
 
 export default function Home ({ open }) {
-  
-  (async () => {
-    const headers = {
-      "Content-Type": "application/json",
-      "x-api-key":"c621b3eb65c60a1372e63a73553d697b202e9c134f9620e23b0acadec922a54fc23b436262a9a00f7c8ce2358dc362a518ee157bc3a118822c0487467210e320676e"
-    
-    }
 
-    const raw = JSON.stringify({
-      questionText: "Where is Criteria Corp?",
-      namespace: "criteria-marketing-prod-v1",
-      companyAccountId: 12345,
-      platformUserId: 12345,
+  (async () => {
+
+    var myHeaders = new Headers();
+    myHeaders.append("x-api-key", "c621b3eb65c60a1372e63a73553d697b202e9c134f9620e23b0acadec922a54fc23b436262a9a00f7c8ce2358dc362a518ee157bc3a118822c0487467210e320676e");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "questionText": "Where ?",
+      "namespace": "criteria-marketing-prod-v1",
+      "companyAccountId": 12345,
+      "platformUserId": 12345
     });
 
-    const requestOptions = {
-      method: "POST",
-      headers: headers,
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
       body: raw,
-      redirect: "follow",
+      redirect: 'follow'
     };
 
-    const response = await fetch(
-      "https://criteria-gpt-chat-ui-git-feature-api-endpo-e28fed-criteria-corp.vercel.app/api/support/SubmitQuestion",
-      requestOptions
-    );
-
-    if (!response.ok) {
-      throw new Error("Request failed with status " + response.status);
-    }
-
-    const data = await response.json();
-    console.log(data);
+    fetch("http://localhost:3000/api/support/SubmitQuestion", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   })()
 
 
@@ -50,7 +43,7 @@ export default function Home ({ open }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout open={open} >
-        <HomeComponent/>
+        <HomeComponent />
       </Layout>
     </div>
   );
